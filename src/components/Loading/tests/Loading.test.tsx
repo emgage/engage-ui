61659a0d9ea2116456c5facc5c7742db514779a5
ranmalr <ranmalr@emgage.com>
2017-06-20 20:45:45 +0530

    Working #30059

--- src/components/Loading/tests/Loading.test.tsx
+++ src/components/Loading/tests/Loading.test.tsx
@@ -8,13 +8,13 @@ const theme = {
 };
 
 describe('<Loading />', () => {
-  describe('when default props are provided', () => {
+  it('when default props are provided', () => {
     const subject = mount(<Loading theme={theme} />);
     expect(subject.find('i')).toHaveLength(1);
-    expect(subject.find('span')).toHaveLength(1);
+    expect(subject.find('span')).toHaveLength(2);
     expect(subject.find('i').hasClass('loader')).toBe(true);
-    expect(subject.find('span').hasClass('srOnly')).toBe(true);
-    expect(subject.find('span').text()).toBe('Loading...');
+    expect(subject.find('span.srOnly')).toHaveLength(1);
+    expect(subject.find('span.srOnly').text()).toBe('Loading...');
   });
 });
 
