23b81ae37dbbabf16f52376e8070f8f4d672b8e3
ranmalr <ranmalr@emgage.com>
2017-07-03 15:45:52 +0530

    Fixed #30099

--- src/components/MaskTextField/defer.ts
+++ src/components/MaskTextField/defer.ts
@@ -1,8 +1,4 @@
 export default function(fn: any) {
-    const defer = window.requestAnimationFrame
-        ||
-        window.webkitRequestAnimationFrame
-        ||
-        (() => setTimeout(fn, 0));
+    const defer = window.requestAnimationFrame || window.webkitRequestAnimationFrame || (() => setTimeout(fn, 0));
     return defer(fn);
 };
