295cd5fd8a00f9a52c769dfe0abf9cfab6e8c866
SandipMehta <sandipm@emgage.com>
2017-07-26 18:48:58 +0530

    Resolved Section issue

--- src/components/Card/tests/Card.test.tsx
+++ src/components/Card/tests/Card.test.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { mount } from 'enzyme';
 import Card from '../Card';
+import Section from '../Section';
 import {Action} from '../../../types';
 
 describe('Card - Test Suit', () => {
@@ -13,57 +14,41 @@ describe('Card - Test Suit', () => {
     };
 
     it('Verify that Card has <h2> tag.', () => {
-        const cardWrapper = mount( <Card    title="Online store dashboard - Card"
-                                            sectioned
-                                            subdued
-                                    >
-                                    <p>View a summary of your online store’s performance.</p>
-                                    </Card>);
+        const cardWrapper = mount( <Card title="Online store dashboard - Card" sectioned subdued>
+                                        <p>View a summary of your online store’s performance.</p>
+                                   </Card>);
         expect(cardWrapper.find('h2').length).toBe(1);
     });
     it('Verify Card title.', () => {
-        const cardWrapper = mount( <Card    title="Online store dashboard - Card"
-                                            sectioned
-                                            subdued
-                                    >
-                                    <p>View a summary of your online store’s performance.</p>
-                                    </Card>);
+        const cardWrapper = mount( <Card title="Online store dashboard - Card" sectioned subdued>
+                                        <p>View a summary of your online store’s performance.</p>
+                                   </Card>);
         expect(cardWrapper.prop('title')).toBe('Online store dashboard - Card');
     });
     it('Verify sectioned when it is set', () => {
-        const cardWrapper = mount( <Card    title="Online store dashboard - Card"
-                                            sectioned
-                                            subdued
-                                    >
-                                    <p>View a summary of your online store’s performance.</p>
-                                    </Card>);
+        const cardWrapper = mount( <Card title="Online store dashboard - Card" sectioned subdued>
+                                        <p>View a summary of your online store’s performance.</p>
+                                   </Card>);
         expect(cardWrapper.prop('sectioned')).toBe(true);
     });
     it('Verify sectioned when it is not set', () => {
-        const cardWrapper = mount( <Card    title="Online store dashboard - Card"
-                                            subdued
-                                    >
-                                    <p>View a summary of your online store’s performance.</p>
-                                    </Card>);
+        const cardWrapper = mount( <Card title="Online store dashboard - Card" subdued>
+                                        <p>View a summary of your online store’s performance.</p>
+                                   </Card>);
         expect(cardWrapper.prop('sectioned')).toBeFalsy();
         cardWrapper.setProps({sectioned: false});
         expect(cardWrapper.prop('sectioned')).toBeFalsy();
     });
     it('Verify subdued is set', () => {
-        const cardWrapper = mount( <Card    title="Online store dashboard - Card"
-                                            sectioned
-                                            subdued
-                                    >
-                                    <p>View a summary of your online store’s performance.</p>
-                                    </Card>);
+        const cardWrapper = mount( <Card title="Online store dashboard - Card" sectioned subdued>
+                                        <p>View a summary of your online store’s performance.</p>
+                                   </Card>);
         expect(cardWrapper.prop('subdued')).toBe(true);
     });
     it('Verify subdued is no set', () => {
-        const cardWrapper = mount( <Card    title="Online store dashboard - Card"
-                                            sectioned
-                                    >
-                                    <p>View a summary of your online store’s performance.</p>
-                                    </Card>);
+        const cardWrapper = mount( <Card title="Online store dashboard - Card" sectioned>
+                                        <p>View a summary of your online store’s performance.</p>
+                                   </Card>);
         expect(cardWrapper.prop('subdued')).toBeFalsy();
     });
 
@@ -79,8 +64,8 @@ describe('Card - Test Suit', () => {
         const spy = jest.fn();
         const action: Action = {content: 'Action Content', onAction: () => {spy(); }};
         const cardWrapper = mount( <Card title="Online store dashboard - Card"  primaryFooterAction={action}  >
-            <p>View a summary of your online store’s performance.</p> 
-            </Card>);
+                                        <p>View a summary of your online store’s performance.</p> 
+                                   </Card>);
         cardWrapper.find('button').simulate('click');
         expect(spy).toHaveBeenCalled();
     });
@@ -88,33 +73,32 @@ describe('Card - Test Suit', () => {
         const spy = jest.fn();
         const action: Action = {content: 'Action Content', onAction: () => {spy(); }};
         const cardWrapper = mount( <Card title="Online store dashboard - Card"  secondaryFooterAction={action}  >
-            <p>View a summary of your online store’s performance.</p> 
-            </Card>);
+                                        <p>View a summary of your online store’s performance.</p> 
+                                   </Card>);
         cardWrapper.find('button').simulate('click');
         expect(spy).toHaveBeenCalled();
     });
     it('Verify sectioned is boolean type', () => {
         const cardWrapper = mount( <Card title="Online store dashboard - Card" sectioned >
-            <p>View a summary of your online store’s performance.</p> 
-            </Card>);
+                                        <p>View a summary of your online store’s performance.</p> 
+                                   </Card>);
         expect(typeof cardWrapper.prop('sectioned')).toBe('boolean');
     });
     it('Verify subdued is boolean type', () => {
         const cardWrapper = mount( <Card title="Online store dashboard - Card" subdued >
-            <p>View a summary of your online store’s performance.</p> 
-            </Card>);
+                                        <p>View a summary of your online store’s performance.</p> 
+                                   </Card>);
         expect(typeof cardWrapper.prop('subdued')).toBe('boolean');
     });
     it('Verify that Card can have multiple sections with title.', () => {
-        const cardWrapper = mount( <Card    title="Online store dashboard - Card"
-                                            sectioned
-                                    >
-                                <Card.Section title="Reports"> 
-                                    <p>View a summary of your online store’s performance.</p>
-                                </Card.Section>
-                                <Card.Section title="Summary Reports">
-                                    <p>View a summary of your online store’s performance, including sales, visitors, top products, and referrals.</p>
-                            </Card.Section></Card>);
+        const cardWrapper = mount(<Card title="Online store dashboard - Card" sectioned theme={theme}>
+                                        <Section title="Reports" theme={theme}> 
+                                            <p>View a summary of your online store’s performance.</p>
+                                        </Section>
+                                        <Section title="Summary Reports" theme={theme}>
+                                            <p>View a summary of your online store’s performance, including sales, visitors, top products, and referrals.</p>
+                                        </Section>
+                                   </Card>);
         expect(cardWrapper.prop('sectioned')).toBe(true);
         expect(cardWrapper.find('Section').at(1).text()).toBe('ReportsView a summary of your online store’s performance.');
         expect(cardWrapper.find('Section').at(2).text()).toBe('Summary ReportsView a summary of your online store’s performance, including sales, visitors, top products, and referrals.');
@@ -122,33 +106,28 @@ describe('Card - Test Suit', () => {
         expect(cardWrapper.find('p').length).toBe(2);
     });
     it('Verify Card with subdued option.', () => {
-        const cardWrapper = mount( <Card    title="Online store dashboard - Card"
-                                            sectioned
-                                    >
-                                        <Card.Section title="Reports" subdued> 
+        const cardWrapper = mount( <Card title="Online store dashboard - Card" sectioned >
+                                        <Section title="Reports" subdued> 
                                             <p>View a summary of your online store’s performance.</p>
-                                        </Card.Section>
-                                        <Card.Section title="Summary Reports" subdued>
+                                        </Section>
+                                        <Section title="Summary Reports" subdued>
                                             <p>View a summary of your online store’s performance, including sales, visitors, top products, and referrals.</p>
-                                        </Card.Section>
-                                    </Card>);
+                                        </Section>
+                                   </Card>);
         expect(cardWrapper.find('h3').length).toBe(2);
         expect(cardWrapper.find('p').length).toBe(2);
         expect(cardWrapper.find('h3').at(0).text()).toBe('Reports');
         expect(cardWrapper.find('h3').at(1).text()).toBe('Summary Reports');
     });
     it('Verify Card with theme.', () => {
-        const cardWrapper = mount( <Card    title="Online store dashboard - Card"
-                                            sectioned
-                                            theme={theme}
-                                    >
-                                        <Card.Section title="Reports" subdued theme={theme}> 
+        const cardWrapper = mount( <Card title="Online store dashboard - Card" sectioned >
+                                        <Section title="Reports" subdued > 
                                             <p>View a summary of your online store’s performance.</p>
-                                        </Card.Section>
-                                        <Card.Section title="Summary Reports" subduedtheme={theme}>
+                                        </Section>
+                                        <Section title="Summary Reports" subdued theme={theme}>
                                             <p>View a summary of your online store’s performance, including sales, visitors, top products, and referrals.</p>
-                                        </Card.Section>
-                                        </Card>);
+                                        </Section>
+                                   </Card>);
         expect(cardWrapper.find('h3').length).toBe(2);
         expect(cardWrapper.find('p').length).toBe(2);
         expect(cardWrapper.find('h3').at(0).text()).toBe('Reports');
