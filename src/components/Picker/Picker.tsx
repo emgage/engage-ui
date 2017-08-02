0cfd12e060bb7491d11ffc53b69fa2018c7634db
Hiren Patel <hirenp@emgage.com>
2017-08-02 16:34:22 +0530

    SearchResult for chips will be displayed on List Component.

--- src/components/Picker/Picker.tsx
+++ src/components/Picker/Picker.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { themr, ThemedComponentClass } from 'react-css-themr';
 import { PICKER } from '../ThemeIdentifiers';
 import TextField from '../TextField';
+import List from '../List';
 import { DisplayMoreInfo } from './PickerEnum';
 
 import * as baseTheme from './Picker.scss';
@@ -95,7 +96,10 @@ class Picker extends React.Component<Props, State> {
                 <div>
                     {
                         this.state.searchItems.map(function (i) {
-                            return React.createElement(searchResultComponent as React.ComponentClass<{ clickable: boolean, moreInfoComponent: React.ReactNode, moreInfoComponentShowOn: DisplayMoreInfo, onClick(item: any): void, handleMoreInfo(): void }>, { key: i.Id, clickable: true, moreInfoComponent, moreInfoComponentShowOn, onClick: onSelect, handleMoreInfo: onMoreInfo }, [i.Name]);
+                            return (<List key={i.Id}>
+                                {React.createElement(searchResultComponent as React.ComponentClass<{ clickable: boolean, moreInfoComponent: React.ReactNode, moreInfoComponentShowOn: DisplayMoreInfo, onClick(item: any): void, handleMoreInfo(): void }>, { key: i.Id, clickable: true, moreInfoComponent, moreInfoComponentShowOn, onClick: onSelect, handleMoreInfo: onMoreInfo }, [i.Name])}
+                            </List>
+                            );
                         })
                     }
                 </div>
