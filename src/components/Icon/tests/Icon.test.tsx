0bbb9f1ac191ca0899edd3fe5332572faacda9c2
DimpleKundanani <dimplek@emgage.com>
2017-08-11 11:31:07 +0530

    Remove redundant icon wrapper from all it statements.

--- src/components/Icon/tests/Icon.test.tsx
+++ src/components/Icon/tests/Icon.test.tsx
@@ -13,74 +13,53 @@ const theme = {
 describe('<Icon />', () => {
 
   describe('when default props are provided', () => {
-    it('basic icon should have rendered one span element', () => {
-      const iconWrapper = mount(
+    const iconWrapper = mount(
                                 <Icon source="notes" theme={theme} />,
                           );
+    it('basic icon should have rendered one span element', () => {
       expect(iconWrapper.find('span')).toHaveLength(1);
     });
     it('basic icon should have rendered with one icon clss on span', () => {
-      const iconWrapper = mount(
-                                <Icon source="notes" theme={theme} />,
-                          );
       expect(iconWrapper.find('span').hasClass('Icon'));
     });
   });
 
   describe('source property', () => {
     describe('when set as svg source', () => {
-      it('basic icon should have rendered one span element', () => {
-        const iconWrapper = mount(
+      const iconWrapper = mount(
                                   <Icon source="notes" theme={theme} />,
                             );
+      it('basic icon should have rendered one span element', () => {
         expect(iconWrapper.find('span')).toHaveLength(1);
       });
       it('basic icon should have rendered with one icon clss on span', () => {
-        const iconWrapper = mount(
-                                  <Icon source="notes" theme={theme} />,
-                            );
         expect(iconWrapper.find('span').hasClass('Icon'));
       });
       it('should verify given source', () => {
-        const iconWrapper = mount(
-                                  <Icon source="notes" theme={theme} />,
-                            );
         expect(iconWrapper.prop('source')).toBe('notes');
       });
     });
 
-      describe('when set as placeholder', () => {
-        it('basic icon should have rendered one span element', () => {
-          const iconWrapper = mount(
-                                    <Icon source="placeholder" theme={theme} />,
-                              );
-          expect(iconWrapper.find('span')).toHaveLength(1);
-        });
-        it('basic icon should have rendered with one icon clss on span', () => {
-          const iconWrapper = mount(
-                                    <Icon source="placeholder" theme={theme} />,
-                              );
-          expect(iconWrapper.find('span').hasClass('Icon'));
-        });
-        it('basic icon should have rendered one div element', () => {
-          const iconWrapper = mount(
-                                    <Icon source="placeholder" theme={theme} />,
-                              );
-          expect(iconWrapper.find('div')).toHaveLength(1);
-        });
-        it('basic icon should have rendered with one placeholder clss on div', () => {
-          const iconWrapper = mount(
-                                    <Icon source="placeholder" theme={theme} />,
-                              );
-          expect(iconWrapper.find('div').hasClass('Placeholder'));
-        });
-        it('should verify given source', () => {
-          const iconWrapper = mount(
-                                    <Icon source="placeholder" theme={theme} />,
-                              );
-          expect(iconWrapper.prop('source')).toBe('placeholder');
-        });
+    describe('when set as placeholder', () => {
+      const iconWrapper = mount(
+                                  <Icon source="placeholder" theme={theme} />,
+                            );
+      it('basic icon should have rendered one span element', () => {
+        expect(iconWrapper.find('span')).toHaveLength(1);
       });
+      it('basic icon should have rendered with one icon clss on span', () => {
+        expect(iconWrapper.find('span').hasClass('Icon'));
+      });
+      it('basic icon should have rendered one div element', () => {
+        expect(iconWrapper.find('div')).toHaveLength(1);
+      });
+      it('basic icon should have rendered with one placeholder clss on div', () => {
+        expect(iconWrapper.find('div').hasClass('Placeholder'));
+      });
+      it('should verify given source', () => {
+        expect(iconWrapper.prop('source')).toBe('placeholder');
+      });
+    });
   });
 
   describe('color property', () => {
@@ -112,28 +91,19 @@ describe('<Icon />', () => {
     });
 
     describe('when not set', () => {
-      it('basic icon should have rendered one span element', () => {
-        const iconWrapper = mount(
+      const iconWrapper = mount(
                                   <Icon source="notes" theme={theme} />,
                             );
+      it('basic icon should have rendered one span element', () => {
         expect(iconWrapper.find('span')).toHaveLength(1);
       });
       it('basic icon should have rendered with one icon clss on span', () => {
-        const iconWrapper = mount(
-                                  <Icon source="notes" theme={theme} />,
-                            );
         expect(iconWrapper.find('span').hasClass('Icon'));
       });
       it('should not have color property', () => {
-        const iconWrapper = mount(
-                                  <Icon source="notes" theme={theme} />,
-                            );
         expect(iconWrapper.find('color')).toHaveLength(0);
       });
       it('should verify color property is not defined', () => {
-        const iconWrapper = mount(
-                                  <Icon source="notes" theme={theme} />,
-                            );
         expect(iconWrapper.prop('color')).toBeUndefined();
       });
     });
@@ -195,28 +165,19 @@ describe('<Icon />', () => {
     });
 
     describe('when not set', () => {
-      it('basic icon should have rendered one span element', () => {
-        const iconWrapper = mount(
+      const iconWrapper = mount(
                                   <Icon source="notes" theme={theme} />,
                             );
+      it('basic icon should have rendered one span element', () => {
         expect(iconWrapper.find('span')).toHaveLength(1);
       });
       it('basic icon should have rendered with one icon clss on span', () => {
-        const iconWrapper = mount(
-                                  <Icon source="notes" theme={theme} />,
-                            );
         expect(iconWrapper.find('span').hasClass('Icon'));
       });
       it('should not have backdrop property', () => {
-        const iconWrapper = mount(
-                                  <Icon source="notes" theme={theme} />,
-                            );
         expect(iconWrapper.find('backdrop')).toHaveLength(0);
       });
       it('should verify backdrop property is not defined', () => {
-        const iconWrapper = mount(
-                                  <Icon source="notes" theme={theme} />,
-                            );
         expect(iconWrapper.prop('backdrop')).toBeUndefined();
       });
     });
@@ -251,28 +212,19 @@ describe('<Icon />', () => {
     });
 
     describe('when not set', () => {
-      it('basic icon should have rendered one span element', () => {
-        const iconWrapper = mount(
+      const iconWrapper = mount(
                                   <Icon source="notes" theme={theme} />,
                             );
+      it('basic icon should have rendered one span element', () => {
         expect(iconWrapper.find('span')).toHaveLength(1);
       });
       it('basic icon should have rendered with one icon clss on span', () => {
-        const iconWrapper = mount(
-                                  <Icon source="notes" theme={theme} />,
-                            );
         expect(iconWrapper.find('span').hasClass('Icon'));
       });
       it('should not have accessibilityLabel property', () => {
-        const iconWrapper = mount(
-                                  <Icon source="notes" theme={theme} />,
-                            );
         expect(iconWrapper.find('accessibilityLabel')).toHaveLength(0);
       });
       it('should verify accessibilityLabel property is not defined', () => {
-        const iconWrapper = mount(
-                                  <Icon source="notes" theme={theme} />,
-                            );
         expect(iconWrapper.prop('accessibilityLabel')).toBeUndefined();
       });
     });
