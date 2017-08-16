a361eef68bbf8944b27b02afb9b8e098d4aca489
SandipMehta <sandipm@emgage.com>
2017-08-16 11:59:21 +0530

    Resolved conflicts

--- src/components/VisuallyHidden/tests/VisuallyHidden.test.tsx
+++ src/components/VisuallyHidden/tests/VisuallyHidden.test.tsx
@@ -4,21 +4,21 @@ import VisuallyHidden from '../VisuallyHidden';
 import Heading from '../../Heading/Heading';
 
 describe ('<VisuallyHidden /> - Test Suit', () => {
-    const theme = {
-        VisuallyHidden: 'VisuallyHidden',
-    };
-    it('should verify that it should have one span tag.', () => {
-        const visuallyHiddenWrapper = mount(<VisuallyHidden theme={theme}>
+  const theme = {
+    VisuallyHidden: 'VisuallyHidden',
+  };
+  it('should verify that it should have one span tag.', () => {
+    const visuallyHiddenWrapper = mount(<VisuallyHidden theme={theme}>
                                                 <Heading>Title and description</Heading>
                                             </VisuallyHidden>,
                                       );
-        expect(visuallyHiddenWrapper.find('span').length).toBe(1);
-    });
-    it('should verify that VisuallyHidden class should be applied to span.', () => {
-        const visuallyHiddenWrapper = mount(<VisuallyHidden theme={theme}>
+    expect(visuallyHiddenWrapper.find('span').length).toBe(1);
+  });
+  it('should verify that VisuallyHidden class should be applied to span.', () => {
+    const visuallyHiddenWrapper = mount(<VisuallyHidden theme={theme}>
                                                 <Heading>Title and description</Heading>
                                             </VisuallyHidden>,
                                       );
-        expect(visuallyHiddenWrapper.find('span').hasClass('VisuallyHidden'));
-    });
+    expect(visuallyHiddenWrapper.find('span').hasClass('VisuallyHidden'));
+  });
 });
