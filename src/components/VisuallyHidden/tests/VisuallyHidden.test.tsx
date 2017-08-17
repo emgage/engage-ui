debb5b4f21f90cfd2a45e977ebfb5bddcbbf351f
Sandip Mehta <sandipm@emgage.com>
2017-08-17 21:57:44 +0530

    Feature/30338 test visually hidden card (#57)
    * 30256 - Card unit test cases.
    
    * Solved Lint Errors
    
    * Resolved Section issue
    
    * Code review fixes for Card
    
    * no message
    
    * no message
    
    * Described the Card and VisuallyHidden component with JSX tags in main describe.
    
    * resolved conflicts
    
    * Resolved conflicts

---
+++ src/components/VisuallyHidden/tests/VisuallyHidden.test.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { mount } from 'enzyme';
+import VisuallyHidden from '../VisuallyHidden';
+import Heading from '../../Heading/Heading';
+
+describe ('<VisuallyHidden /> - Test Suit', () => {
+  const theme = {
+    VisuallyHidden: 'VisuallyHidden',
+  };
+  it('should verify that it should have one span tag.', () => {
+    const visuallyHiddenWrapper = mount(<VisuallyHidden theme={theme}>
+                                                <Heading>Title and description</Heading>
+                                            </VisuallyHidden>,
+                                      );
+    expect(visuallyHiddenWrapper.find('span').length).toBe(1);
+  });
+  it('should verify that VisuallyHidden class should be applied to span.', () => {
+    const visuallyHiddenWrapper = mount(<VisuallyHidden theme={theme}>
+                                                <Heading>Title and description</Heading>
+                                            </VisuallyHidden>,
+                                      );
+    expect(visuallyHiddenWrapper.find('span').hasClass('VisuallyHidden'));
+  });
+});
