8fc450d1a2b6d782ed903a43d72ab2ffe8fb587c
Jonathan Kim <kimhjona@gmail.com>
2017-08-21 16:52:01 -0700

    reached some milestones

--- src/components/Picker/AutoSuggestTest.tsx
+++ src/components/Picker/AutoSuggestTest.tsx
@@ -4,30 +4,18 @@ import Card from './Card';
 import * as style from './Picker.scss';
 import Chip from '../Chip';
 
-const languages = [
-  { key: 1, image: 'http://msaadvertising.com/wp-content/uploads/2014/06/Larry-cartoon-headshot.jpg', name: 'John Doe', email: 'test@gmail.com', grey: false },
-  { key: 2, image: 'http://cdn.photographyproject.com.au/wp-content/uploads/2013/04/corporate-headshot.jpg', name: 'Pedro Sanchez', email: 'pedrosanchez@gmail.com', grey: false },
-  { key: 3, image: 'https://media.licdn.com/mpr/mpr/p/5/005/08f/04d/02df10d.jpg', name: 'Jane Doe', email: 'jane@gmail.com', grey: false },
-  { key: 4, image: 'http://www.roanokecreditrepair.com/wp-content/uploads/2016/06/Headshot-1.png', name: 'Person McPerson', email: 'yahoogmail@gmail.com', grey: false },
-  { key: 5, image: 'https://d38zhw9ti31loc.cloudfront.net/wp-content/uploads/2013/07/Crystal-headshot-new.jpg', name: 'Laura Person', email: 'yahooldjadslkjgmail@gmail.com', grey: false },
-  { key: 6, image: 'https://d38zhw9ti31loc.cloudfront.net/wp-content/uploads/2013/07/Crystal-headshot-new.jpg', name: 'Laura Person', email: 'slkjgmail@gmail.com', grey: false },
-];
+// const languages = [
+//   { key: 1, image: 'http://msaadvertising.com/wp-content/uploads/2014/06/Larry-cartoon-headshot.jpg', name: 'John Doe', email: 'test@gmail.com', grey: false },
+//   { key: 2, image: 'http://cdn.photographyproject.com.au/wp-content/uploads/2013/04/corporate-headshot.jpg', name: 'Pedro Sanchez', email: 'pedrosanchez@gmail.com', grey: false },
+//   { key: 3, image: 'https://media.licdn.com/mpr/mpr/p/5/005/08f/04d/02df10d.jpg', name: 'Jane Doe', email: 'jane@gmail.com', grey: false },
+//   { key: 4, image: 'http://www.roanokecreditrepair.com/wp-content/uploads/2016/06/Headshot-1.png', name: 'Person McPerson', email: 'yahoogmail@gmail.com', grey: false },
+//   { key: 5, image: 'https://d38zhw9ti31loc.cloudfront.net/wp-content/uploads/2013/07/Crystal-headshot-new.jpg', name: 'Laura Person', email: 'yahooldjadslkjgmail@gmail.com', grey: false },
+//   { key: 6, image: 'https://d38zhw9ti31loc.cloudfront.net/wp-content/uploads/2013/07/Crystal-headshot-new.jpg', name: 'Laura Person', email: 'slkjgmail@gmail.com', grey: false },
+// ];
 
 // https://developer.mozilla.org/en/docs/Web/JavaScript/Guide/Regular_Expressions#Using_Special_Characters
 function escapeRegexCharacters(str:any) {
   return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-}
-
-function getSuggestions(value:any) {
-  const escapedValue = escapeRegexCharacters(value.trim());
-  
-  if (escapedValue === '') {
-    return [];
-  }
-
-  const regex = new RegExp('^' + escapedValue, 'i');
-
-  return languages.filter(language => regex.test(language.name));
 }
 
 function renderSuggestion(suggestion:any) {
@@ -44,6 +32,7 @@ export interface State {
   chipListState: { key: any, image?: string, text: string, email?: string, grey?: boolean }[];
   value: string;
   suggestions: object[];
+  languages: { key: any, image?: string, email?: string, grey?: boolean, name: any }[];
 }
 
 class AutoSuggestTest extends React.Component<Props, State> {
@@ -54,16 +43,36 @@ class AutoSuggestTest extends React.Component<Props, State> {
       value: '',
       suggestions: [],
       chipListState: [
-        { key: 'just a chip!!!!!!!!!!', text: 'just a chip' },
-        { key: 'just another chip', text: 'just another chip' },
+      ],
+      languages: [
+        { key: 1, image: 'http://msaadvertising.com/wp-content/uploads/2014/06/Larry-cartoon-headshot.jpg', name: 'John Doe', email: 'test@gmail.com', grey: false },
+        { key: 2, image: 'http://cdn.photographyproject.com.au/wp-content/uploads/2013/04/corporate-headshot.jpg', name: 'Pedro Sanchez', email: 'pedrosanchez@gmail.com', grey: false },
+        { key: 3, image: 'https://media.licdn.com/mpr/mpr/p/5/005/08f/04d/02df10d.jpg', name: 'Jane Doe', email: 'jane@gmail.com', grey: false },
+        { key: 4, image: 'http://www.roanokecreditrepair.com/wp-content/uploads/2016/06/Headshot-1.png', name: 'Person McPerson', email: 'yahoogmail@gmail.com', grey: false },
+        { key: 5, image: 'https://d38zhw9ti31loc.cloudfront.net/wp-content/uploads/2013/07/Crystal-headshot-new.jpg', name: 'Laura Person', email: 'yahooldjadslkjgmail@gmail.com', grey: false },
+        { key: 6, image: 'https://d38zhw9ti31loc.cloudfront.net/wp-content/uploads/2013/07/Crystal-headshot-new.jpg', name: 'Laura Person', email: 'slkjgmail@gmail.com', grey: false },
       ],
     };
+  }
+  getSuggestions = (value:any) => {
+    const escapedValue = escapeRegexCharacters(value.trim());
+    
+    if (escapedValue === '') {
+      return [];
+    }
+
+    const regex = new RegExp('^' + escapedValue, 'i');
+
+    return this.state.languages.filter(language => regex.test(language.name));
   }
 
 
   getSuggestionValue = (suggestion:any) => {
-    const chipListState = this.state.chipListState.concat({ key: suggestion.name, text: suggestion.name });
+    suggestion.text = suggestion.name;
+    const chipListState = this.state.chipListState.concat(suggestion);
     this.setState({ chipListState });
+    this.updateList(suggestion);
+    // this.languages.splice(languages.indexOf(suggestion), 1);
     // this.setState({ suggestions: [] });
     // this.state.chipListState.push({ text: suggestion.name });
     return '';
@@ -76,7 +85,7 @@ class AutoSuggestTest extends React.Component<Props, State> {
   
   onSuggestionsFetchRequested = ({ value }:any) => {
     this.setState({
-      suggestions: getSuggestions(value),
+      suggestions: this.getSuggestions(value),
     });
   }
 
@@ -84,6 +93,41 @@ class AutoSuggestTest extends React.Component<Props, State> {
     this.setState({
       suggestions: [],
     });
+  }
+
+  chipClick = () => {
+    console.log('chip clicked...');
+  }
+
+  updateList = (input: any) => {
+    const langIndex = this.state.languages.indexOf(input);
+    console.log('langIndex:', langIndex);
+    const languagesLength = this.state.languages;
+    console.log('languagesLength:', languagesLength);
+    const newLangState = languagesLength.slice(0, langIndex).concat(languagesLength.slice(langIndex + 1, languagesLength.length));
+    console.log('newLangState:', newLangState);
+
+    this.setState({ languages: newLangState });
+  }
+
+  chipRemove = (input:any) => {
+    console.log('input:', input);
+    const index = this.state.chipListState.indexOf(input);
+    console.log('index:', index);
+    const chipLength = this.state.chipListState;
+    const newChipState = chipLength.slice(0, index).concat(chipLength.slice(index + 1, chipLength.length));
+
+    console.log('newChipState:', newChipState);
+    this.setState({ chipListState: newChipState });
+
+    const languagesList = this.state.languages.concat(input);
+    this.setState({ languages: languagesList });
+
+    // console.log('remove this:', this);
+
+    console.log('chip removed...');
+
+    // this.updateList(input);
   }
 
   render() {
@@ -94,18 +138,10 @@ class AutoSuggestTest extends React.Component<Props, State> {
       onChange: this.onChange,
     };
 
-    // const chipList = (chipListState: any) => (
-    //   <div>
-    //     {chipListState.map((station: any) => (
-    //       <Chip key={station.text}>{station.text}</Chip>
-    //     ))}
-    //   </div>
-    // );
-
     return (
       <div>
         <div>
-          { chipListState.map((input: any) => <div key={input.text}><Chip key={input.text}>{input.text}</Chip></div>) }
+          { chipListState.map((input: any) => <div key={input.text}><Chip removable={true} onRemove={() => this.chipRemove(input)} key={input.text}>{input.text}</Chip></div>) }
         </div>
         <Autosuggest 
           suggestions={suggestions}
