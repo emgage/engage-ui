a10ba88a6a5adf870fa312a84e19811dde936ce9
DimpleKundanani <dimplek@emgage.com>
2017-08-25 17:26:10 +0530

    features/30757-Tooltip-Documentation

--- example/src/reducers/InitialState.ts
+++ example/src/reducers/InitialState.ts
@@ -2,11 +2,13 @@ import { IDocumentAppState } from '../Types';
 
 import ChoiceListState from './componentState/ChoiceListState';
 import PanelState from './componentState/PanelState';
+import TooltipState from './componentState/TooltipState';
 
 const intialState : IDocumentAppState = {
   components: [
     ChoiceListState,
     PanelState,
+    TooltipState,
   ],
 };
 
