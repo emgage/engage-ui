4aba8f3b7c8d480c3e46b4d402b4b0699852e1ef
DevanshiGandhi <devanshig@emgage.com>
2017-08-25 19:34:49 +0530

    features/30756-TextField-Documentation

---
+++ example/src/examples/TextFieldExample/TextFieldExampleFirst.tsx
@@ -0,0 +1,115 @@
+import * as React from 'react';
+import { TextField , ChoiceList , FormLayout , Select  } from '../../../../src/components/';
+
+import * as styles from '../../styles/components-page.scss';
+
+export interface IProps{
+}
+
+export interface IState {
+}
+
+class TextFieldExampleFirst extends React.Component<IProps> {
+  constructor(props: IProps) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  valueUpdater(field: any) {
+    return (value: any) => this.setState({ [field]: value });
+  }
+
+  render() {
+    return (
+      <div className={styles.example}>
+        1. Default TextField with label prop:
+        <TextField label="TextField Label" />
+        <br/>
+        2. Number Field with value prop:
+        <TextField
+          label=""
+          type="number"
+          value="1"
+        />
+        <br/>
+        3. Email Field:
+        <TextField
+          label=""
+          type="email"
+        />
+        <br/>
+        4. Multiline TextField:
+        <TextField
+          label="Shipping address"
+          multiline
+        />
+        <br/>
+        5. TextField with Hidden Label:
+        <FormLayout>
+          <ChoiceList
+            title="Gift card auto-expiration"
+            choices={[
+              {
+                label: 'Gift cards never expire',
+                value: 'no',
+              },
+              {
+                label: 'Gift cards expire',
+                value: 'yes',
+              },
+            ]}
+            selected={['yes']}
+          />
+          <TextField
+            label="Gift cards expire after"
+            labelHidden
+            value="12"
+            connectedRight={<Select label="Unit of time" labelHidden options={['months after purchase']} />}
+          />
+        </FormLayout>
+        <br/>
+      6. TextField with Label Action:
+        <TextField
+          label="Tariff code"
+          labelAction={{ content: 'Look up codes' }}
+        />
+        <br/>
+      7. TextField with placeholder text:
+        <TextField
+          label="Zone name"
+          placeholder="e.g. North America, Europe"
+        />
+        <br/>
+      8. TextField with Help Text:
+        <TextField
+          label="Account email"
+          type="email"
+          helpText="We’ll use this address if we need to contact you about your account."
+        />
+        <br/>
+      9. TextField with prefix or Suffix:
+        <TextField
+          label="Price"
+          type="number"
+          prefix="$"
+        />
+        <br/>
+      10. TextField with connected Fields:
+        <TextField
+          label="Weight"
+          type="number"
+          connectedRight={<Select label="Weight unit" labelHidden options={[
+            'kg',
+            'lb',
+          ]} />}
+        />
+        <br/>
+      11. TextField with Validation Error:
+        <TextField errors={['This field is mandatory.']} label="TextField" />
+        </div>
+    );
+  }
+}
+
+export default TextFieldExampleFirst;
--- example/src/reducers/InitialState.ts
+++ example/src/reducers/InitialState.ts
@@ -2,11 +2,13 @@ import { IDocumentAppState } from '../Types';
 
 import ChoiceListState from './componentState/ChoiceListState';
 import PanelState from './componentState/PanelState';
+import TextFieldState from './componentState/TextFieldState';
 
 const intialState : IDocumentAppState = {
   components: [
     ChoiceListState,
     PanelState,
+    TextFieldState,
   ],
 };
 
