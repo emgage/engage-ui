e196cdffbe718209f09b622faad67dfb2609b285
DevanshiGandhi <devanshig@emgage.com>
2017-08-25 16:48:36 +0530

    features/30751-Scrollable-Documentation

---
+++ example/src/examples/ScrollableExample/ScrollableExampleFirst.tsx
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import { Scrollable } from '../../../../src/components/';
+import * as styles from '../../styles/components-page.scss';
+
+export interface IProps{
+}
+
+export interface IState {
+}
+
+class ScrollableExampleFirst extends React.Component<IProps> {
+  constructor(props: IProps) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  valueUpdater(field: any) {
+    return (value: any) => this.setState({ [field]: value });
+  }
+
+  render() {
+    return (
+      <div className={styles.example}>
+        1. Default Scrollable Container:
+        <Scrollable>
+        <p>By signing up for the Shopify service (“Service”) or
+            any of the services of Shopify Inc. (“Shopify”) you are agreeing to be bound by
+            the following terms and conditions (“Terms of Service”). The Services offered by Shopify
+            under the Terms of Service include various products and services to help you create and manage
+            a retail store, whether an online store (“Online Services”), a physical retail store
+            (“POS Services”), or both. Any new features or tools which are added to the current Service
+            shall be also subject to the Terms of Service. You can review the current version of the Terms of
+            Service at any time at https://www.shopify.com/legal/terms.</p>
+        </Scrollable>
+        <br/>
+
+        2. Scrollable Container with prop:
+        <Scrollable shadow vertical horizontal
+        style={{ height: '100px' }}>
+        <p>By signing up for the Shopify service (“Service”) or
+            any of the services of Shopify Inc. (“Shopify”) you are agreeing to be bound by
+            the following terms and conditions (“Terms of Service”). The Services offered by Shopify
+            under the Terms of Service include various products and services to help you create and manage
+            a retail store, whether an online store (“Online Services”), a physical retail store
+            (“POS Services”), or both. Any new features or tools which are added to the current Service
+            shall be also subject to the Terms of Service. You can review the current version of the Terms of
+            Service at any time at https://www.shopify.com/legal/terms.</p>
+        </Scrollable>
+
+      </div>
+    );
+  }
+}
+
+export default ScrollableExampleFirst;
--- example/src/reducers/InitialState.ts
+++ example/src/reducers/InitialState.ts
@@ -2,11 +2,13 @@ import { IDocumentAppState } from '../Types';
 
 import ChoiceListState from './componentState/ChoiceListState';
 import PanelState from './componentState/PanelState';
+import ScrollableState from './componentState/ScrollableState';
 
 const intialState : IDocumentAppState = {
   components: [
     ChoiceListState,
     PanelState,
+    ScrollableState,
   ],
 };
 
