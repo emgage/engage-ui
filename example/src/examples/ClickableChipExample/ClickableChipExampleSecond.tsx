3a7ba6ab405ec7becc1643e47da23d3e947f9fda
DimpleKundanani <dimplek@emgage.com>
2017-08-25 18:50:45 +0530

    features/30731-ClickableChip-Documentation

---
+++ example/src/examples/ClickableChipExample/ClickableChipExampleSecond.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import { ClickableChip ,Chip ,Card } from '../../../../src/components/';
+import * as styles from '../../styles/components-page.scss';
+
+export interface IProps{
+}
+
+export interface IState {
+}
+
+class ClickableChipExampleSecond extends React.Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+
+    this.state = {
+    };
+  }
+
+  valueUpdater(field: any) {
+    return (value: any) => this.setState({ [field]: value });
+  }
+
+  render() {
+    return (
+      <div className={styles.example}>
+        <h3>2.ClickableChip with detail on click:</h3>
+        <ClickableChip chip={<Chip>Batman</Chip>}>
+          <Card title="More about Batman">
+            <p>Batman is a fictional superhero who appears in American comic books published by DC Comics.
+              The character was created by artist Bob Kane and writer Bill Finger, and first appeared in Detective Comics #27</p>
+          </Card>
+        </ClickableChip>
+      </div>
+    );
+  }
+}
+
+export default ClickableChipExampleSecond;
