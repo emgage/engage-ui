560428a74b90c2061fa7b6bc7a1eecfd09bbd31d
DevanshiGandhi <devanshig@emgage.com>
2017-08-28 15:31:19 +0530

    features/30755-Tag-Documentation

--- example/src/reducers/InitialState.ts
+++ example/src/reducers/InitialState.ts
@@ -2,11 +2,13 @@ import { IDocumentAppState } from '../Types';
 
 import ChoiceListState from './componentState/ChoiceListState';
 import PanelState from './componentState/PanelState';
+import TagState from './componentState/TagState';
 
 const intialState : IDocumentAppState = {
   components: [
     ChoiceListState,
     PanelState,
+    TagState,
   ],
 };
 
