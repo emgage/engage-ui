7227de58309c4a8e6d3b90df8c84de325e96a5b5
HardikShah <hardiks@emgage.com>
2017-08-28 15:11:03 +0530

    features/30722-Badge-Documentation

---
+++ example/src/examples/BadgeExample/BadgeExampleFirst.tsx
@@ -0,0 +1,75 @@
+import * as React from 'react';
+import { Badge } from '../../../../src/components/';
+import * as styles from '../../styles/components-page.scss';
+
+export interface IProps{
+}
+
+export interface IState {
+}
+
+class BadgeExample extends React.Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+
+    this.state = {
+    };
+  }
+
+  valueUpdater(field: any) {
+    return (value: any) => this.setState({ [field]: value });
+  }
+
+  render() {
+    return (
+      <div className={styles.example}>
+        <h3>1. Default badge:</h3>
+        <br/>
+          <Badge>
+            Badge Example 1
+          </Badge>
+        <br/>
+        <br/>
+        <h3>2. Badge with progress=complete and status=success:</h3>
+        <br/>
+          <Badge
+            progress="complete"
+            status="success"
+          >
+            Badge Example 2
+          </Badge>
+        <br/>
+        <br/>
+        <h3>3. Badge with progress=partiallyComplete and status=info:</h3>
+        <br/>
+          <Badge
+            progress="partiallyComplete"
+            status="info"
+          >
+            Badge Example 3
+          </Badge>
+        <br/>
+        <br/>
+        <h3>4. Badge with progress=incomplete and status=attention:</h3>
+        <br/>
+          <Badge
+            progress="incomplete"
+            status="attention"
+          >
+            Badge Example 4
+          </Badge>
+          <br/>
+          <br/>
+        <h3>5. Badge with status=warning and without progress:</h3>
+        <br/>
+          <Badge
+            status="warning"
+          >
+            Badge Example 5
+          </Badge>
+      </div>
+    );
+  }
+}
+
+export default BadgeExample;
--- example/src/reducers/InitialState.ts
+++ example/src/reducers/InitialState.ts
@@ -2,11 +2,13 @@ import { IDocumentAppState } from '../Types';
 
 import ChoiceListState from './componentState/ChoiceListState';
 import PanelState from './componentState/PanelState';
+import BadgeState from './componentState/BadgeState';
 
 const intialState : IDocumentAppState = {
   components: [
     ChoiceListState,
     PanelState,
+    BadgeState,
   ],
 };
 
