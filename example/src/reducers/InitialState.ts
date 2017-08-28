b04ddbf42ccb943f6953cb0aebd4bc2d4990a3c7
SandipMehta <sandipm@emgage.com>
2017-08-28 15:51:46 +0530

    Loading Component Docuentation

--- example/src/reducers/InitialState.ts
+++ example/src/reducers/InitialState.ts
@@ -2,11 +2,13 @@ import { IDocumentAppState } from '../Types';
 
 import ChoiceListState from './componentState/ChoiceListState';
 import PanelState from './componentState/PanelState';
+import LoadingState from './componentState/LoadingState';
 
 const intialState : IDocumentAppState = {
   components: [
     ChoiceListState,
     PanelState,
+    LoadingState,
   ],
 };
 
