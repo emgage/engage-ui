8997fd5940cb901a7d37f83338657d8317d6cd85
SandipMehta <sandipm@emgage.com>
2017-08-28 14:46:28 +0530

    Chip component documentation

--- example/src/reducers/InitialState.ts
+++ example/src/reducers/InitialState.ts
@@ -2,11 +2,13 @@ import { IDocumentAppState } from '../Types';
 
 import ChoiceListState from './componentState/ChoiceListState';
 import PanelState from './componentState/PanelState';
+import ChipState from './componentState/ChipState';
 
 const intialState : IDocumentAppState = {
   components: [
     ChoiceListState,
     PanelState,
+    ChipState,
   ],
 };
 
