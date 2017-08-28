8d42abb41c3a1fbefcec4a33db00f25f751c89b8
HardikShah <hardiks@emgage.com>
2017-08-28 16:38:27 +0530

    features/30745-Message-Documentation

---
+++ example/src/examples/MessageExample/MessageExampleFirst.tsx
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import { Message, Button } from '../../../../src/components/';
+import * as styles from '../../styles/components-page.scss';
+
+export interface IProps{
+}
+
+export interface IState {
+  isvisibled: boolean;
+}
+
+class MessageExample extends React.Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+
+    this.state = {
+      isvisibled: false,
+    };
+  }
+
+  valueUpdater(field: any) {
+    return (value: any) => this.setState({ [field]: value });
+  }
+
+  render() {
+    return (
+      <div className={styles.example}>
+        <h3>1. Show message when isVisible=true:</h3>
+        <br/>
+        <Message
+          id = "1010101010"
+          isVisible
+        >
+          Test message
+        </Message>
+        <br/>
+        <h3>2. Show message by clicking button:</h3>
+        <br/>
+        <Message
+          id = "1010101010"
+          isVisible = {this.state.isvisibled}
+        >
+          This is your message
+        </Message>
+        <Button
+          onClick={() => this.setState({ isvisibled: !this.state.isvisibled })}
+        >
+          View / Hide Message
+        </Button>
+        <br/>
+      </div>
+    );
+  }
+}
+
+export default MessageExample;
--- example/src/reducers/InitialState.ts
+++ example/src/reducers/InitialState.ts
@@ -2,11 +2,13 @@ import { IDocumentAppState } from '../Types';
 
 import ChoiceListState from './componentState/ChoiceListState';
 import PanelState from './componentState/PanelState';
+import MessageState from './componentState/MessageState';
 
 const intialState : IDocumentAppState = {
   components: [
     ChoiceListState,
     PanelState,
+    MessageState,
   ],
 };
 
