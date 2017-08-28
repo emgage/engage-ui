8783a044505e51b73e497c51ea9125c51edb83c8
DimpleKundanani <dimplek@emgage.com>
2017-08-28 15:00:35 +0530

    features/30762-VisuallyHidden -Documentation.

--- example/src/reducers/InitialState.ts
+++ example/src/reducers/InitialState.ts
@@ -2,11 +2,13 @@ import { IDocumentAppState } from '../Types';
 
 import ChoiceListState from './componentState/ChoiceListState';
 import PanelState from './componentState/PanelState';
+import VisuallyHiddenState from './componentState/VisuallyHiddenState';
 
 const intialState : IDocumentAppState = {
   components: [
     ChoiceListState,
     PanelState,
+    VisuallyHiddenState,
   ],
 };
 
