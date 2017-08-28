34e04633b26f99b48a83087f4a86abead3652f53
HardikShah <hardiks@emgage.com>
2017-08-28 15:33:54 +0530

    features/30727-Checkbox-Documentation

---
+++ example/src/examples/CheckBoxExample/CheckBoxExampleSecond.tsx
@@ -0,0 +1,59 @@
+import * as React from 'react';
+import { Checkbox } from '../../../../src/components/';
+import * as styles from '../../styles/components-page.scss';
+
+export interface IProps{
+}
+
+export interface IState {
+
+}
+
+class CheckboxExample extends React.Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+
+    this.state = {
+    };
+  }
+
+  valueUpdater(field: any) {
+    return (value: any) => this.setState({ [field]: value });
+  }
+
+  render() {
+    return (
+      <div className={styles.example}>
+        <h3>4. Checkbox with helpText:</h3>
+        <Checkbox
+          label="With helpText "
+          helpText="Some help"
+          id="MyCheckbox"
+          name="checkbox"
+          value="Some value"
+        />
+        <h3>5. Checkbox with error:</h3>
+        <Checkbox
+          label="With error"
+          id="MyCheckbox"
+          name="checkbox"
+          value="Some value"
+          error="Some error"
+        />
+        <h3>6.Checkbox with all properties:</h3>
+        <Checkbox
+          label="Checkbox"
+          labelHidden
+          checked
+          helpText="Some help"
+          id="MyCheckbox"
+          name="checkbox"
+          value="Some value"
+          error="Some error" disabled={false}
+        />
+      </div>
+    );
+  }
+}
+
+export default CheckboxExample;
--- example/src/reducers/InitialState.ts
+++ example/src/reducers/InitialState.ts
@@ -2,11 +2,13 @@ import { IDocumentAppState } from '../Types';
 
 import ChoiceListState from './componentState/ChoiceListState';
 import PanelState from './componentState/PanelState';
+import CheckboxState from './componentState/CheckboxState';
 
 const intialState : IDocumentAppState = {
   components: [
     ChoiceListState,
     PanelState,
+    CheckboxState,
   ],
 };
 
