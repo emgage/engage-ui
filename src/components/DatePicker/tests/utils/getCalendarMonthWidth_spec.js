97132427c608f5815bc725e11e77e6408001d24c
Jonathan Kim <kimhjona@gmail.com>
2017-08-29 14:57:22 -0700

    removed testing file to modularize pull requests

--- src/components/DatePicker/tests/utils/getCalendarMonthWidth_spec.js
+++
@@ -1,9 +0,0 @@
-import { expect } from 'chai';
-
-import getCalendarMonthWidth from '../../src/utils/getCalendarMonthWidth';
-
-describe('#getCalendarMonthWidth', () => {
-  it('correctly calculates width for default day size of 39', () => {
-    expect(getCalendarMonthWidth(39)).to.equal(300);
-  });
-});
