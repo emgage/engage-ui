f2eb58f8e00d416597eacfb3f518ae0684ea2d30
SandipMehta <sandipm@emgage.com>
2017-08-29 18:29:19 +0530

    Validated TextField documentation.

--- example/src/reducers/InitialState.ts
+++ example/src/reducers/InitialState.ts
@@ -2,11 +2,14 @@ import { IDocumentAppState } from '../Types';
 
 import ChoiceListState from './componentState/ChoiceListState';
 import PanelState from './componentState/PanelState';
+import ValidatedTextFieldState from './componentState/ValidatedTextFieldState';
+
 
 const intialState : IDocumentAppState = {
   components: [
     ChoiceListState,
     PanelState,
+    ValidatedTextFieldState,
   ],
 };
 
