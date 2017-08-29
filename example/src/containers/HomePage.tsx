ae81127d6155adef4a7e0b307e360fd5017086a6
Jonathan Kim <kimhjona@gmail.com>
2017-08-29 14:16:59 -0700

    compiling but does not render anything

--- example/src/containers/HomePage.tsx
+++ example/src/containers/HomePage.tsx
@@ -5,12 +5,16 @@ import '../styles/home-page.scss';
 
 // tslint:disable-next-line:import-name
 import * as styles from '../styles/global-styles.scss';
+import DateRangePickerWrapper from '../DateRangePickerWrapper';
+import SingleDatePickerWrapper from '../SingleDatePickerWrapper';
 
 
 const HomePage = () => {
   return (
       <div className={styles.component_container}>
         <div className={styles.homepage}> 
+          <DateRangePickerWrapper />
+          <SingleDatePickerWrapper />
           <Heading value="Engage-UI" />
           <p>This is the home or root page for the engage-ui docs</p>
           <p>Just some text about how amazing the engage-ui library truely is</p>
---
+++ src/components/DatePicker/index.ts
@@ -0,0 +1,53 @@
+// import undefined from '../../../lib/@types/src/components/DatePicker/utils/toMomentObject';
+// import moment from 'moment';
+import DateRangePicker from './components/DateRangePicker';
+import DateRangePickerInput from './components/DateRangePickerInput';
+import DateRangePickerInputController from './components/DateRangePickerInputController';
+import SingleDatePicker from './components/SingleDatePicker';
+import SingleDatePickerInput from './components/SingleDatePickerInput';
+import DayPicker from './components/DayPicker';
+import DayPickerRangeController from './components/DayPickerRangeController';
+import DayPickerSingleDateController from './components/DayPickerSingleDateController';
+import CalendarMonthGrid from './components/CalendarMonthGrid';
+import CalendarMonth from './components/CalendarMonth';
+import CalendarDay from './components/CalendarDay';
+
+import DateRangePickerShape from './shapes/DateRangePickerShape';
+import SingleDatePickerShape from './shapes/SingleDatePickerShape';
+
+import isInclusivelyAfterDay from './utils/isInclusivelyAfterDay';
+import isInclusivelyBeforeDay from './utils/isInclusivelyBeforeDay';
+import isNextDay from './utils/isNextDay';
+import isSameDay from './utils/isSameDay';
+
+import toISODateString from './utils/toISODateString';
+import toLocalizedDateString from './utils/toLocalizedDateString';
+import toMomentObject from './utils/toMomentObject';
+
+export {
+  DateRangePicker,
+  SingleDatePicker,
+
+  DateRangePickerInputController,
+  DateRangePickerInput,
+  SingleDatePickerInput,
+  DayPicker,
+  DayPickerRangeController,
+  DayPickerSingleDateController,
+  CalendarMonthGrid,
+  CalendarMonth,
+  CalendarDay,
+
+  DateRangePickerShape,
+  SingleDatePickerShape,
+
+  isInclusivelyAfterDay,
+  isInclusivelyBeforeDay,
+  isNextDay,
+  isSameDay,
+
+  toISODateString,
+  toLocalizedDateString,
+  // toMomentObject: moment(toMomentObject),
+  toMomentObject,
+};
