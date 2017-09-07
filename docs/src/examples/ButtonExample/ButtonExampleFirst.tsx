afee99cc7947e46246babcec1f6f05c8d2d591c1
DevanshiGandhi <devanshig@emgage.com>
2017-09-07 17:50:17 +0530

    Fix review changes.

--- docs/src/examples/ButtonExample/ButtonExampleFirst.tsx
+++ docs/src/examples/ButtonExample/ButtonExampleFirst.tsx
@@ -17,52 +17,52 @@ class ButtonExampleFirst extends React.Component<IProps> {
 
   render() {
     return (
-        <div className={styles.example}>
-          1. Basic Button:
-          <br/>
-          <Button>Basic Button</Button>
-          <br/><br/>
-          2. Outline Button:
-          <br/>
-          <Button outline>Outline Button</Button>
-          <br/><br/>
-          3.Plain Button with url and external prop:
-          <br/>
-          <Button plain url = "http://www.lipsum.com/" external>Plain Button</Button>
-          <br/><br/>
-          4. Primary Button:
-          <br/>
-          <Button primary>Primary Button</Button>
-          <br/><br/>
-          5. Destructive Button with accessibilityLabel prop:
-          <br/>
-          <Button destructive accessibilityLabel="No Label">Destructive Button</Button>
-          <br/><br/>
-          6. Slim Button:
-          <br/>
-          <Button size="slim">Slim Button</Button>
-          <br/><br/>
-          7. Large Button with disclosure prop:
-          <br/>
-          <Button size="large" disclosure={true}>Large Button</Button>
-          <br/><br/>
-          8. FullWidth Button:
-          <br/>
-          <Button fullWidth>FullWidth Button</Button>
-          <br/><br/>
-          9. Disabled Button:
-          <br/>
-          <Button disabled>Disabled Button</Button>
-          <br/><br/>
-          10. Button with Submit prop:
-          <br/>
-          <Button submit={true}>Submit Button</Button>
-          <br/><br/>
-          11. Button with icon prop:
-          <br/>
-          <Button icon="calendar"></Button>
-          <br/><br/>
-        </div>
+      <div className={styles.example}>
+        1. Basic Button:
+        <br/>
+        <Button>Basic Button</Button>
+        <br/><br/>
+        2. Outline Button:
+        <br/>
+        <Button outline>Outline Button</Button>
+        <br/><br/>
+        3.Plain Button with url and external prop:
+        <br/>
+        <Button plain url = "http://www.lipsum.com/" external>Plain Button</Button>
+        <br/><br/>
+        4. Primary Button:
+        <br/>
+        <Button primary>Primary Button</Button>
+        <br/><br/>
+        5. Destructive Button with accessibilityLabel prop:
+        <br/>
+        <Button destructive accessibilityLabel="No Label">Destructive Button</Button>
+        <br/><br/>
+        6. Slim Button:
+        <br/>
+        <Button size="slim">Slim Button</Button>
+        <br/><br/>
+        7. Large Button with disclosure prop:
+        <br/>
+        <Button size="large" disclosure={true}>Large Button</Button>
+        <br/><br/>
+        8. FullWidth Button:
+        <br/>
+        <Button fullWidth>FullWidth Button</Button>
+        <br/><br/>
+        9. Disabled Button:
+        <br/>
+        <Button disabled>Disabled Button</Button>
+        <br/><br/>
+        10. Button with Submit prop:
+        <br/>
+        <Button submit={true}>Submit Button</Button>
+        <br/><br/>
+        11. Button with icon prop:
+        <br/>
+        <Button icon="calendar"></Button>
+        <br/><br/>
+      </div>
     );
   }
 }
