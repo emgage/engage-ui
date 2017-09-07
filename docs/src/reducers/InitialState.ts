25f7c27034cd22d5c3e6ca7064b62d8e203342a9
DevanshiGandhi <30464746+DevanshiGandhi@users.noreply.github.com>
2017-09-08 02:40:19 +0530

    features/30741-Labelled-Documentation (#112)
    * features/30741-Labelled-Documentation
    
    * Fix the review changes.

--- docs/src/reducers/InitialState.ts
+++ docs/src/reducers/InitialState.ts
@@ -2,12 +2,14 @@ import { IDocumentAppState } from '../Types';
 
 import ChoiceListState from './componentState/ChoiceListState';
 import PanelState from './componentState/PanelState';
+import LabelledState from './componentState/LabelledState';
 import PositionedOverlayState  from './componentState/PositionedOverlayState';
 
 const intialState : IDocumentAppState = {
   components: [
     ChoiceListState,
     PanelState,
+    LabelledState,
     PositionedOverlayState,
   ],
 };
