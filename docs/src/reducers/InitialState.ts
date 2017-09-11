2bae38d3464f6bb71d19bee25ebe664d222a0d71
Sandip Mehta <sandipm@emgage.com>
2017-09-11 14:42:00 +0530

    Card component documentation (#71)
    * Card component documentation
    
    * Updated Card Description and Properties

--- docs/src/reducers/InitialState.ts
+++ docs/src/reducers/InitialState.ts
@@ -2,6 +2,7 @@ import { IDocumentAppState } from '../Types';
 
 import ChoiceListState from './componentState/ChoiceListState';
 import PanelState from './componentState/PanelState';
+import CardState from './componentState/CardState';
 import LabelledState from './componentState/LabelledState';
 import LabelState from './componentState/LabelState';
 import PositionedOverlayState  from './componentState/PositionedOverlayState';
@@ -10,6 +11,7 @@ const intialState : IDocumentAppState = {
   components: [
     ChoiceListState,
     PanelState,
+    CardState,
     LabelledState,
     LabelState,
     PositionedOverlayState,
