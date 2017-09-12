a9dc03041b736e67814e51a566ee3c27a5f986ba
hardiksemgage <30464893+hardiksemgage@users.noreply.github.com>
2017-09-12 11:35:55 +0530

    features/30732-Column-Documentation (#106)
    * features/30732-Column-Documentation
    
    * Rename Index.tsx to index.tsx
    
    * Rename Index.tsx to index.tsx
    
    * Rename Index.tsx to index.tsx
    
    * Rename Index.tsx to index.tsx
    
    * Rename Index.tsx to index.tsx
    
    * Rename Index.ts to index.ts
    
    * Rename ConfigureStore.ts to configurestore.ts
    
    * Rename configurestore.ts to configureStore.ts

--- docs/src/components/Nav/index.tsx
+++ docs/src/components/Nav/index.tsx
@@ -12,7 +12,9 @@ const Nav = () => {
         <li><IndexLink to="/">Home</IndexLink></li>
         <li><Link to="/component/choicelist" className={styles.active}>Choice List Component</Link></li>
         <li><Link to="/component/panel">Panel Component</Link></li>
+        <li><Link to="/component/column">Column Component</Link></li>
         <li><Link to="/component/badge">Badge Component</Link></li>
+        <li><Link to="/component/column">Column Component</Link></li>
         <li><Link to="/component/loading">Loading Component</Link></li>
         <li><Link to="/component/select">Select Component</Link></li>
         <li><Link to="/component/connected">Connected Component</Link></li>
--- docs/src/reducers/InitialState.ts
+++ docs/src/reducers/InitialState.ts
@@ -2,6 +2,7 @@ import { IDocumentAppState } from '../Types';
 
 import ChoiceListState from './componentState/ChoiceListState';
 import PanelState from './componentState/PanelState';
+import ColumnState from './componentState/ColumnState';
 import ConnectedState from './componentState/ConnectedState';
 import BadgeState from './componentState/BadgeState';
 import LoadingState from './componentState/LoadingState';
@@ -15,6 +16,7 @@ const intialState : IDocumentAppState = {
   components: [
     ChoiceListState,
     PanelState,
+    ColumnState,
     ConnectedState,
     BadgeState,
     LoadingState,
