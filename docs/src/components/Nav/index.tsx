185201b55c799af88bead964b483d35af1d1f727
DevanshiGandhi <30464746+DevanshiGandhi@users.noreply.github.com>
2017-09-20 15:23:43 +0530

    features/30751-Scrollable-Documentation (#81)
    * features/30751-Scrollable-Documentation
    
    * Rename Index.tsx to index.tsx

--- docs/src/components/Nav/index.tsx
+++ docs/src/components/Nav/index.tsx
@@ -10,6 +10,7 @@ const Nav = () => {
         <li><IndexLink to="/">Home</IndexLink></li>
         <li><Link to="/component/choicelist" className={styles.active}>Choice List Component</Link></li>
         <li><Link to="/component/panel">Panel Component</Link></li>
+        <li><Link to="/component/Scrollable">Scrollable Component</Link></li>
         <li><Link to="/component/Link">Link Component</Link></li>
         <li><Link to="/component/TextField">TextField Component</Link></li>
         <li><Link to="/component/FlexBox">FlexBox Component</Link></li>
--- docs/src/reducers/InitialState.ts
+++ docs/src/reducers/InitialState.ts
@@ -2,6 +2,7 @@ import { IDocumentAppState } from '../Types';
 
 import ChoiceListState from './componentState/ChoiceListState';
 import PanelState from './componentState/PanelState';
+import ScrollableState from './componentState/ScrollableState';
 import LinkState from './componentState/LinkState';
 import TextFieldState from './componentState/TextFieldState';
 import FlexBoxState from './componentState/FlexBoxState';
@@ -30,6 +31,7 @@ const intialState : IDocumentAppState = {
   components: [
     ChoiceListState,
     PanelState,
+    ScrollableState,
     TextFieldState,
     FlexBoxState,
     FormLayoutState,
