36d922c24dc7f17643d3bb38868e7cfe68ebe72a
Hiren Patel <hirenp@emgage.com>
2017-10-03 11:09:55 +0530

    31867_modal_overflow_prop_bug_with example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { } from '../../src/components/Modal/Dialog';
 import { PeoplePickerSearchType } from './PickerEnum';
 import { PeoplePickerSource } from './PickerSource';
 
@@ -7,6 +8,8 @@ import ReactDataExample from './ReactDataExample';
 import SingleDatePickerWrapper from './SingleDatePickerWrapper';
 import DateRangePickerWrapper from './DateRangePickerWrapper';
 import PickerAutoSuggestExample from './PickerAutoSuggestExample';
+import * as velocity from 'velocity-animate';
+import { ITrigger, IAnimate } from '../../src/components/Modal/Modal';
 
 import {
   Button,
@@ -37,6 +40,7 @@ import {
   ValidatedForm,
   Video,
   VideoType,
+  Modal,
 } from '../../src/components';
 
 interface State {
@@ -46,9 +50,21 @@ interface State {
   columns: object[];
   rows: object[];
   isMenuOpened: boolean;
+  show: boolean;
 }
 
 class App extends React.Component<{}, State> {
+
+  animate: IAnimate = {
+    in: (modal, dialog) => this.animateIn(modal, dialog),
+    out: (modal, dialog) => this.animateOut(modal, dialog),
+  };
+
+  triggers: ITrigger = {
+    body: 'Open',
+    animate: this.animate,
+  };
+
   constructor(props: any) {
     super(props);
     this.state = {
@@ -66,7 +82,20 @@ class App extends React.Component<{}, State> {
         { id: 3, title: 'Title 3', count: 3 },
       ],
       isMenuOpened: false,
+      show: false,
     };
+  }
+
+  animateIn(modal?: Element, dialog?: Element): void {
+    this.setState({ show: true });
+    velocity(modal, { opacity: 1 }, { display: 'block' }, 300);
+    velocity(dialog, { translateY: 1, opacity: 1 }, { display: 'block' }, 200);
+  }
+
+  animateOut(modal?: Element, dialog?: Element): void {
+    this.setState({ show: false });
+    velocity(modal, { opacity: 0 }, { display: 'none' }, 300);
+    velocity(dialog, { translateY: -100, opacity: 0 }, { display: 'none' }, 200);
   }
 
   rowGetter = (index: number) => this.state.rows[index];
@@ -109,6 +138,77 @@ class App extends React.Component<{}, State> {
 
     return (
       <div>
+        <div>
+        <h1>This is my Modal Component!!</h1>
+          <Modal
+          id="test1"
+            close
+            closeOnBackgroud = {true}
+            closeOnEsc
+            modalOverflow = {true}
+            backdropEnabled
+            header="This is my modal header"
+            footer={<Button>OK</Button>}
+            activator={<Button>Modal</Button>}
+            size={500}
+            show={this.state.show}
+            trigger={this.triggers}
+          >
+
+            <h2>Headline test</h2>
+            <p>
+              test Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in
+
+
+            test Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in
+
+            test Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in
+            test Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in
+            test Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in
+
+            test Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in
+
+            test Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in
+
+            test Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in
+
+            test Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in
+
+            test Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in
+              </p>
+
+          </Modal>
+      </div>
         <PickerAutoSuggestExample />
         <SingleDatePickerWrapper />
         <DateRangePickerWrapper />
