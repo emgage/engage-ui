4ff2e921bda8513c2c88a63c58ca18830d6051ec
Shyam Kanojia <shyamk@emgage.com>
2017-10-31 19:57:42 +0530

    Implemented documentation changes in Avatar and Badge

--- docs/src/examples/BadgeExample/BadgeExampleSeventh.tsx
+++ docs/src/examples/BadgeExample/BadgeExampleSeventh.tsx
@@ -4,8 +4,10 @@ import * as styles from '../../styles/components-page.scss';
 
 const BadgeExample = () => (
   <div className={styles.example}>
-    <Badge>
-      Badge Example 1
+    <Badge
+      progress="partiallyComplete"
+    >
+      Badge Example 7
     </Badge>
   </div>
 );
--- docs/src/examples/BadgeExample/BadgeExampleThird.tsx
+++ docs/src/examples/BadgeExample/BadgeExampleThird.tsx
@@ -4,8 +4,10 @@ import * as styles from '../../styles/components-page.scss';
 
 const BadgeExample = () => (
   <div className={styles.example}>
-    <Badge>
-      Badge Example 1
+    <Badge
+      status="info"
+    >
+      Badge Example 3
     </Badge>
   </div>
 );
