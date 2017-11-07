ee77524b802d3ca1f8d25cd51245141e3bc2b2b9
Todd <toddc@emgage.com>
2017-11-07 13:16:22 -0800

    Cleaning up with Loading and Icon docs.

--- src/components/Icon/images/index.ts
+++
@@ -1,2 +0,0 @@
-// tslint:disable-next-line:eofline
-export { default as avatarOne } from './spinner.svg';
--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -6,14 +6,11 @@ import VisuallyHidden from '../VisuallyHidden';
 import { LOADING } from '../ThemeIdentifiers';
 
 import * as baseTheme from './Loading.scss';
-import * as loading from '../Icon/images';
 
 export interface Props {
   style?: React.CSSProperties;
   theme?: any;
 }
-
-const LOADING_IMAGES = Object.keys(loading).map((key: keyof typeof loading) => loading[key]);
 
 class Loading extends React.PureComponent<Props, {}> {
   render() {
@@ -24,7 +21,7 @@ class Loading extends React.PureComponent<Props, {}> {
 
     return (
             <div style={style}>
-                <span className={theme.loading}><Icon source={LOADING_IMAGES[0]} backdrop /></span>
+                <span className={theme.loading}><Icon source="spinner" /></span>
                 <VisuallyHidden>Loading...</VisuallyHidden>
             </div>
     );
