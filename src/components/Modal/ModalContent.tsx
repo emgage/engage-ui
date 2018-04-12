a1fc9f8b0aad12ab8f8c524487f66a307e4e519c
Dheeraj Agrawal <dheerajagrawal00@gmail.com>
2018-04-12 15:00:14 +0530

    Modal stacking changes done

---
+++ src/components/Modal/ModalContent.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import { themr, ThemedComponentClass } from 'react-css-themr';
+
+import { MODAL } from '../ThemeIdentifiers';
+
+import * as baseTheme from './Modal.scss';
+
+export interface Props {
+  active?: boolean;
+  id?: string;
+  theme?: any;
+}
+
+class ModalContent extends React.Component<Props, never> {
+  constructor(props: Props) {
+    super(props);
+  }
+
+  render() {
+    const { active, children } = this.props;
+
+    return (
+      <div>
+        {
+          active ? children : null
+        }
+      </div>
+    );
+  }
+}
+
+export default themr(MODAL, baseTheme)((ModalContent)) as ThemedComponentClass<Props, {}>;
