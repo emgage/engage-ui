5c5ecbc21fc07f8884d74aa082087a0b74ef7fa6
Ishani Majmundar <ishani-m>
2018-05-03 16:28:41 +0530

    #35427: Did styling of button

--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -2,8 +2,6 @@ import * as React from 'react';
 import { themr, ThemedComponentClass } from 'react-css-themr';
 import { ALERT } from '../ThemeIdentifiers';
 import * as baseTheme from './Alert.scss';
-import Icon from '../Icon';
-import closeSvg from './icons/close.svg';
 
 export interface State {
   isActive?:boolean;
@@ -13,10 +11,15 @@ export interface Props {
   children?: React.ReactNode;
   type?: string;
   theme?: any;
-  onClose?(): void;
-  toggle?(): void;
 }
-  
+const IconClose = (props:any) => {
+    return (
+      <svg width="12" height="12" viewBox="0 0 14 14" xmlns="http://www.w3.org/2000/svg">
+        <line stroke={props.color} stroke-width="1.1" x1="1" y1="1" x2="13" y2="13"></line>
+        <line stroke={props.color} stroke-width="1.1" x1="13" y1="1" x2="1" y2="13"></line>
+      </svg>
+    )
+ };
   class Alert extends React.Component<Props, State> {
     constructor(props: Props) {
       super(props);
@@ -26,28 +29,33 @@ export interface Props {
     }
     render() {
       const { children, type, theme } = this.props;
-      let classes : string, style : string;
+      let classes : string, style : string, color : string;
       switch (type) {
         case 'primary':
           style = theme.alertPrimary;
+          color = "#1e87f0";
         break;
         case 'success':
           style = theme.alertSuccess;
+          color = "#32d296";
           break;
         case 'warning' :
           style = theme.alertWarning;
+          color = "#faa05a";
           break;
         case 'danger':
           style = theme.alertDanger;
+          color = "#f0506e";
           break;
         default:
           style = '';
+          color = '#000';
       }
       classes = theme.alert +' '+ style;
       return (
         <React.Fragment>
         { this.state.isActive && <div className={classes}>
-          <p>{children}</p><button type="button" className={theme.alertClose} onClick={() => this.closeAlert()}><Icon source={closeSvg} /></button>
+          <p>{children}</p><button type="button" className={theme.alertClose} onClick={() => this.closeAlert()}><IconClose color={color}/></button>
         </div>}
         </React.Fragment>
       );
