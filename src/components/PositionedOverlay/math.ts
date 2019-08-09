0ec8edec41ac68a45b72db4b8876f25575e1fc5c
Suhag Lapani <webdev.lapani88@gmail.com>
2019-08-09 13:14:00 +0530

    fix: id:41675 fix popover alignment issue

--- src/components/PositionedOverlay/math.ts
+++ src/components/PositionedOverlay/math.ts
@@ -87,7 +87,7 @@ export function calculateHorizontalPosition(
   preferredPosition: PreferredPosition,
   preloadedPopover: boolean
 ) {
-  const maximum = containerRect.width - overlayRect.width;
+  const maximum = containerRect.width - overlayRect.width - overlayMargins.horizontal;
   // Define when overlay needs to be displayed left aligned with dropdown's left side
   if (preferredAlignment === 'left') {
 
