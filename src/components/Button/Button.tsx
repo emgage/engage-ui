7818a650aed25abbeb0447f275e509b0e3364cdc
HirenTariwala <hirentariwala19@gmail.com>
2020-03-26 13:42:18 +0530

    fix: id:44311 set children as ReactNode

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -14,6 +14,8 @@ export type Size = 'slim' | 'large';
 export interface Props {
   // To display the URL link.
   url?: string;
+  // The content to display inside the button.
+  children?: React.ReactNode;
   // Change the size of the button. It can be slim or large
   componentSize?: Size;
   // Allows the button to grow to the width of its container
