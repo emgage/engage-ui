f521824f55e745bbe0842b3e37185c2ecbb3e9c5
Farhan Girach <farhang@emgage.com>
2020-05-20 10:54:36 +0530

    fix: id:44714 style add/update audience screen

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -1,9 +1,14 @@
 import * as React from 'react';
 import { themr, ThemedComponentClass } from '@friendsofreactjs/react-css-themr';
 import { HeadingTagName } from '../../types';
-import { classNames } from '@shopify/react-utilities/styles';
+import { classNames, variationName } from '@shopify/react-utilities/styles';
 import { HEADING } from '../ThemeIdentifiers';
 import * as baseTheme from './Heading.scss';
+
+export type Size = 'h1' | 'h2' | 'h3' | 'h4' | 'subtitle';
+
+export type Color = 'text';
+
 export interface Props {
   componentClass?: string;
   componentStyle?: any;
@@ -11,6 +16,10 @@ export interface Props {
   element?: HeadingTagName;
   // The content to display inside the heading.
   children?: React.ReactNode;
+  // Size of the text small, medium, large or extralarge
+  headingSize?: Size;
+  // Color of component
+  headingColor?: Color;
   // Theme to be injected via css-themr.
   theme?: any;
 }
@@ -19,12 +28,16 @@ const heading = ({
   componentClass = '',
   componentStyle = {},
   element: ELEMENT = 'h2',
+  headingSize = 'subtitle',
+  headingColor = 'text',
   children,
   theme,
 }: Props) => {
   const headingClass = classNames(
     componentClass,
-    theme.heading
+    theme.heading,
+    headingSize && theme[variationName('size', headingSize)],
+    headingColor && theme[variationName('color', headingColor)]
   );
 
   return <ELEMENT className={headingClass} style={componentStyle}>{children}</ELEMENT>;
