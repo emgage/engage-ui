dcd67d5857651311b664941db576bc99ced035fc
Smit Vora <smit.vora1@gmail.com>
2020-12-06 00:48:15 +0530

    fix: validateDOMNesting(...): error fix for table

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -284,7 +284,7 @@ class Table extends React.PureComponent<Props, State> {
               );
             })
           }
-          { rowAction && !rowActionLeft ? <TableHead key="headRowAction" theme={theme} /> : '' }
+          { rowAction && !rowActionLeft && <TableHead key="headRowAction" theme={theme} />}
         </TableRow>
       </TableHeader>
     );
@@ -400,7 +400,7 @@ class Table extends React.PureComponent<Props, State> {
             })
           }
 
-          { rowAction && !rowActionLeft ? <TableData componentClass={theme.lastData} componentStyle={{ float: 'right' }}>{item.isRowLoading && <Spinner componentSize="small" componentColor="disabled" />} <RowAction componentId={componentId} actionInProgress={actionInProgress && !!item.processing} isRowLoading={item.isRowLoading} actionConfig={rowAction} data={item} theme={theme} /> </TableData> : '' }
+          { rowAction && !rowActionLeft && <TableData componentClass={theme.lastData} componentStyle={{ float: 'right' }}>{item.isRowLoading && <Spinner componentSize="small" componentColor="disabled" />} <RowAction componentId={componentId} actionInProgress={actionInProgress && !!item.processing} isRowLoading={item.isRowLoading} actionConfig={rowAction} data={item} theme={theme} /> </TableData> }
         </TableRow>
         { renderBanner &&
         <TableRow>
