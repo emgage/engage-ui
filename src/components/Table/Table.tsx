5e74b7f487eb8c0f2a1dcecf173493eb3955e617
Smit Vora <smit.vora1@gmail.com>
2020-12-08 19:26:54 +0530

    fix: 48198- selectedRow issue

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -146,12 +146,7 @@ class Table extends React.PureComponent<Props, State> {
     }
 
     if (newProps.data.length !== this.props.data.length && JSON.stringify(newProps.data) !== JSON.stringify(this.props.data)) {
-      let { selectedRows } = this.state;
-      if (selectedRows.length) {
-        const allItemId = newProps.data.map((item: any) => item.id);
-        selectedRows = selectedRows.filter(selectedItem => allItemId.includes(selectedItem));
-      }
-      this.setState({ selectedRows, data: newProps.data });
+      this.setState({ data: newProps.data });
     }
 
     if (JSON.stringify(newProps.nestedChildData) !== JSON.stringify(this.props.nestedChildData)) {
