33048ea0a32c7f2848e71de6978e909219ad77e0
DhruvinPatel <patel.dhruvinpatel@gmail.com>
2021-01-26 14:49:16 +0530

    id:49877 add minusMinor icon alias

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -201,6 +201,7 @@ export const BUNDLED_ICONS = {
   delete: deleteIcon,
   export: exportIcon,
   import: importIcon,
+  minusIcon: minusMinor,
 };
 
 const COLORS_WITH_BACKDROPS = ['teal', 'tealDark', 'greenDark', 'redDark', 'yellowDark', 'ink'];
