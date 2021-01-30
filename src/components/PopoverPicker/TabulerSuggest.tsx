5281820ff0685e5895781d318c48d461716ab465
Sagar Vaghela <sagar19vaghela.sv@email.com>
2021-01-30 10:33:31 +0530

    fix: id: 50073 fixed z index issue

--- src/components/PopoverPicker/TabulerSuggest.tsx
+++ src/components/PopoverPicker/TabulerSuggest.tsx
@@ -90,11 +90,11 @@ class TabulerSuggest extends React.PureComponent<Props, State> {
   };
   
   componentDidMount() {
-    // document.addEventListener('click', this.handleClickOutside);
+    document.addEventListener('click', this.handleClickOutside);
   }
 
   componentWillUnmount() {
-    // document.removeEventListener('click', this.handleClickOutside);
+    document.removeEventListener('click', this.handleClickOutside);
   }
 
   componentWillReceiveProps(nextProps: any) {
