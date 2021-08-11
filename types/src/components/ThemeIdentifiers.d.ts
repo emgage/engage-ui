7c274f857b1a1b0e25b3f92a922e82690e110ae3
Automated VSTS Build Server <buildagent@emgage.com>
2021-08-11 07:24:02 +0000

    chore: Release [skip ci]

--- types/src/components/ThemeIdentifiers.d.ts
+++ types/src/components/ThemeIdentifiers.d.ts
@@ -67,3 +67,4 @@ export declare const PAGESIZE = "PPageSize";
 export declare const SWITCHCHECKBOX = "PSwitchCheckbox";
 export declare const AUTOSUGGEST = "PAutosuggest";
 export declare const SEARCH_HELPER = "PSearchHelper";
+export declare const NODATA = "PNoData";
