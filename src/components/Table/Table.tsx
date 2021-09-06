b956f4907dbfdc203f3999429b6951c59fbfb598
emgage-smit <smitv@emgage.com>
2021-09-06 14:13:28 +0530

    Next: fix-Table-subText

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -448,12 +448,12 @@ class Table extends React.PureComponent<Props, State> {
                   {colItem.injectBody ? colItem.injectBody(item) : renderCheckbox ?
                     <span style={{ paddingLeft: '16px' }}>
                       {item[colItem.key]}
-                      {colItem.subKey && <BodyText componentSize="small" componentColor="mid">{item[colItem.subKey]}</BodyText>}
+                      {colItem.subKey && item[colItem.subKey] && <BodyText componentClass={theme.subText} componentSize="small" componentColor="mid">{item[colItem.subKey]}</BodyText>}
                     </span>
                     :
                     <span className={theme.tableDataWrap}>
                       {item[colItem.key]}
-                      {colItem.subKey && <BodyText componentSize="small" componentColor="mid">{item[colItem.subKey]}</BodyText>}
+                      {colItem.subKey && item[colItem.subKey] && <BodyText componentClass={theme.subText} componentSize="small" componentColor="mid">{item[colItem.subKey]}</BodyText>}
                     </span>
                   }
                   {item.isRowLoading && <Spinner componentSize="small" componentColor="disabled" />}
