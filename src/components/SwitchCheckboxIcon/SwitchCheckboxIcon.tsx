4cc57db566e4c0defe252068195b48a76acebc7e
Farhan-emgage <farhang@emgage.com>
2021-09-23 15:28:13 +0530

    remove unwanted consoles and add children prop

--- src/components/SwitchCheckboxIcon/SwitchCheckboxIcon.tsx
+++ src/components/SwitchCheckboxIcon/SwitchCheckboxIcon.tsx
@@ -24,11 +24,9 @@ export interface Props {
   allowNull?:boolean;
 }
 
-// const getUniqueID = createUniqueIDFactory('SwitchCheckboxIcon');
-
 const SwitchCheckboxIcon = (props: Props) => {
-  const { theme, componentClass, componentStyle, /*componentId = getUniqueID(),*/isOpen, allowNull = true } = props;
-  const { /*children, isOpen, disabled, */handleToggle, switchType= 'normal' } = props;
+  const { theme, componentClass, componentStyle, isOpen, allowNull = true } = props;
+  const { children, handleToggle, switchType= 'normal' } = props;
   let switchTypeClass = theme.switchNormal;
   switch (switchType) {
     case 'normal':
@@ -54,28 +52,27 @@ const SwitchCheckboxIcon = (props: Props) => {
         typeof isOpen !== 'boolean' && !isOpen && theme.nullSelect ,
         allowNull && theme.disableSwitch
     );
-  console.log(isOpen);
   return (<div style={componentStyle}  className={className} aria-label="Switch">
-      <div className={theme.outeWrap}>
+            <div className={theme.outeWrap}>
 
-      <label htmlFor="falseRadio" className={theme.falseRadio}>
-            <input type="radio" checked={isOpen === false} name="switch" id="falseRadio" onClick={() => handleToggle(false)}/>
-            <span className={theme.switchRadio}>false</span>
-            <Icon source="cancel" componentColor="inkLighter" componentClass={theme.Cancel}></Icon>
-        </label>
+            <label htmlFor="falseRadio" className={theme.falseRadio}>
+                  <input type="radio" checked={isOpen === false} name="switch" id="falseRadio" onClick={() => handleToggle(false)}/>
+                  <span className={theme.switchRadio}>false</span>
+                  <Icon source="cancel" componentColor="inkLighter" componentClass={theme.Cancel}></Icon>
+              </label>
 
-        <label htmlFor="nullRadio" className={theme.nullRadio}>
-          <input type="radio" name="switch" id="nullRadio" checked={typeof isOpen !== 'boolean' && !isOpen} disabled={!allowNull} onClick={() => allowNull && handleToggle()}/>
-          <span className={theme.switchRadio}>null</span>
-        </label>
+              <label htmlFor="nullRadio" className={theme.nullRadio}>
+                <input type="radio" name="switch" id="nullRadio" checked={typeof isOpen !== 'boolean' && !isOpen} disabled={!allowNull} onClick={() => allowNull && handleToggle()}/>
+                <span className={theme.switchRadio}>null</span>
+              </label>
 
-        <label htmlFor="trueRadio" className={theme.trueRadio}>
-            <input type="radio" checked={isOpen === true} name="switch" id="trueRadio" onClick={() => handleToggle(true)}/>
-            <span className={theme.switchRadio}>True</span>
-            <Icon source="check" componentColor="blue" componentClass={theme.Accept}></Icon>
-        </label>
-      </div>
-
+              <label htmlFor="trueRadio" className={theme.trueRadio}>
+                  <input type="radio" checked={isOpen === true} name="switch" id="trueRadio" onClick={() => handleToggle(true)}/>
+                  <span className={theme.switchRadio}>True</span>
+                  <Icon source="check" componentColor="blue" componentClass={theme.Accept}></Icon>
+              </label>
+            </div>
+            {children && <label className={theme.switchLabel}>{children}</label>}
         </div>);
 };
 
