a458a5c164d4996f448aa43c3793edb64ce35c2a
Farhan <farhang@emgage.com>
2021-11-25 10:23:49 +0530

    fix: id:55109 change option count color in facet component

--- src/components/MultipleCheckboxFacets/MultipleCheckboxFacets.tsx
+++ src/components/MultipleCheckboxFacets/MultipleCheckboxFacets.tsx
@@ -4,6 +4,7 @@ import Button from '../Button';
 import Checkbox from '../Checkbox';
 import TextField from '../TextField';
 import { Card, CardBody } from '../Card';
+import BodyText from '../BodyText';
 
 function getNewClassName(newClassName: string) {
   if (!Array.isArray(newClassName)) return newClassName;
@@ -93,8 +94,8 @@ function multiCheckboxFacet({
                   }
                 />
               </div>
-              <div style={{ display: 'flex', alignItems: 'center', color: '#9e9e9e' }} className="facet-option-count">
-                {option.count && option.count.toLocaleString('en')}
+              <div style={{ display: 'flex', alignItems: 'center' }} className="facet-option-count">
+                <BodyText element="span" componentColor="mid">{option.count && option.count.toLocaleString('en')}</BodyText>
               </div>
             </div>
           );
