603d114fd332574eed378ec49e0fdb3a47611202
weegrom <toddc@emgage.com>
2022-01-24 10:50:44 -0800

    Update TableData.tsx

--- test/data/TableData.tsx
+++ test/data/TableData.tsx
@@ -5,4 +5,8 @@ export const TableData = [
   { project: "Project 2", comments: 0, owner: "Jane Cooper", image: "JaneCooper.jpg", status: "Healthy", timeline: "Jan 8 - Apr 30", budget: "$154,000", priority: "high", stage: "In Progress", completed: 50},
   { project: "Project 4", comments: 0, owner: "Kristin Watson", image: "KristinWatson.jpg", status: "At Risk", timeline: "Feb 8 - Mar 12", budget: "$25,000", priority: "high", stage: "In Progress", completed: 70},
   { project: "Project 5", comments: 3, owner: "Trevor Hansen", image: "TrevorHansen.jpg", status: "Healthy", timeline: "Jan 25 - Apr 8", budget: "$5,000", priority: "medium", stage: "In Progress", completed: 50},
+  { project: "Project 7", comments: 0, owner: "Cody Fisher", image: "CodyFisher.jpg", status: "Stuck", timeline: "Feb 15 - Feb 22", budget: "$12,500", priority: "medium", stage: "In Progress", completed: 70},
+  { project: "Project 8", comments: 1, owner: "Jane Hancock", image: "JaneHancock.jpg", status: "Healthy", timeline: "Feb 22 - Mar 12", budget: "$9,000", priority: "low", stage: "In Progress", completed: 50},
+  { project: "Project 3", comments: 0, owner: "Eleanor Pena", image: "EleanorPena.jpg", status: "Done", timeline: "Jan 4 - Feb 12", budget: "$12,000", priority: "high", stage: "Completed", completed: 100},
+  { project: "Project 1", comments: 0, owner: "Dianne Russel", image: "DianneRussel.jpg", status: "Done", timeline: "Jan 1 - Feb 1", budget: "$18,000", priority: "medium", stage: "Completed", completed: 100}
 ];
